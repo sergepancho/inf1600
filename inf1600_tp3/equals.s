54c8d696afb0d45ce25d4bb97d3206db4bb61d55
Fabrice Ralph Ndui Ntoutoume <fandua@l4712-20.info.polymtl.ca>
2018-11-08 23:01:42 -0500

    fonction egale semble fonctionner

--- inf1600_tp3/equals.s
+++ inf1600_tp3/equals.s
@@ -20,24 +20,44 @@ matrix_equals_asm:
 			
 			BOUCLE2:
 				TESTC:
-				cmp %ecx, -8(%ebp) /* il compare c avec matorder */
+				mov -8(%ebp), %edx  /* il sauvegarde c dans edx*/
+				cmp %ecx, -8(%ebp) /* il compare matorder avec c  */
 				jge ENDBOUCLE2 /* sort de la boucle si matorder <= c */
 				
-				IF:
-				/*r * matorder*/
-				/*c + r * matorder*/
-				/*inmatdata1[c + r * matorder]*/
-				/*inmatdata2[c + r * matorder]*/
-				/*compare legalite*/
-				je  /* sort si cest egale*/
-				add $1, -8(%ebp)  /* incremente c */
-				jump TESTC /* revient sur testc */
+					IF:
+										
+					/* calcul des valeurs a linterieur du tableau */
+					mov -4(%ebp), %eax  /* met r dans eax*/
+					mul %ecx  /*r * matorder*/
+					add -8(%ebp),%eax  /*c + r * matorder*/
+					mov %eax, %edi /* sauvegarde du calcul de c + r * matorder*/
+					
+					/* dereference les tableaux */
+					mov 8(%ebp), %eax /*adresse des tableau*/
+					mov 12(%ebp), %esi	/*adresse des tableau*/
+					
+					mov (%eax, %edi, 4), %eax  /*inmatdata2[c + r * matorder]*/
+					mov (%esi, %edi, 4), %esi
+					
+					/* fais la comparaison */
+					add $1, -8(%ebp)  /* incremente c juste avant la comparaison */
+
+					cmp %esi,%eax  /* compare legalite*/
+					je TESTC	/* on retourne faire le test si cest egale */
+					
+					mov $0, %eax /*retourne 0*/
+					jmp ENDFONCTION
+				
+				
+				jmp TESTC /* revient sur testc */
 			ENDBOUCLE2: 
 			
-		add $1, -4(%ebp)	/* incremente r */
-		jump TESTR
+			add $1, -4(%ebp)	/* incremente r */
+			jmp TESTR
         
         ENDBOUCLE1:
+			mov $1, %eax /*retourne 1*/
+        ENDFONCTION:
         
         leave          /* Restore ebp and esp */
         ret            /* Return to the caller */
