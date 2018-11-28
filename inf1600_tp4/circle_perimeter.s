4c29a116daa96021d44e7218183fd58081088712
fnduister <fnduister@gmail.com>
2018-11-28 06:14:44 -0500

    merde

--- inf1600_tp4/circle_perimeter.s
+++ inf1600_tp4/circle_perimeter.s
@@ -1,6 +1,5 @@
 .data
         factor: .float 2.0 /* use this to multiply by two */
-        pi: .float 3.14 /*creaction de pi*/
 
 .text
 .globl _ZNK7CCircle12PerimeterAsmEv
@@ -9,14 +8,14 @@ _ZNK7CCircle12PerimeterAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         mov 8(%ebp), %edi /*sauvegarde de this*/
-        lea 8(%edi), %esi /*sauvegarde de l'adresse de raduis*/
-        fld (%esi) /*mise sur la pile de la valeur de raduis pour la multiplication st[0] */
-        fld (%esi) /*raduis dans st[1]*/
+        lea 4(%edi), %eax /*sauvegarde de l'adresse de raduis*/
+        fld factor /*mise sur la pile de la valeur de raduis pour la multiplication st[0] */
+        fld (%eax) /*raduis dans st[1]*/
         fmulp /*multiplie st[0] et st[1]*/
         fldpi /*ajout de pi sur la pile*/
         fmulp /* multiplie pi * raduis * raduis */
         fstp (%eax)
         /* Write your solution here */
-        
+        mov (%eax), %eax
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
