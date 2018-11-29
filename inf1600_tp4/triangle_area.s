e3faad8ba2268adf11b637d64c50e8b7e4eb6bb2
fnduister <fnduister@gmail.com>
2018-11-29 02:48:11 -0500

    termine

--- inf1600_tp4/triangle_area.s
+++ inf1600_tp4/triangle_area.s
@@ -9,14 +9,41 @@ _ZNK9CTriangle7AreaAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
+        addl $-8, %esp /*espace pour la variable local p et espace qu'on va utiliser pour les calculs*/
         mov 8(%ebp), %edi /*sauvegarde de this*/
+        lea 4(%edi), %eax /*sauvegarde de l'adresse du tableau mSide*/
+        push %edi /*sauvegarde this dans la pile*/
         mov (%edi), %edi /*vtable adresse*/
-        call *12(%edi) /*sauvegarde de l'adresse de raduis*/
-        fld factor /*mise sur la pile de la valeur de raduis pour la multiplication st[0] */
-        fmulp /*multiplie st[0] et st[1]*/
-        fldpi /*ajout de pi sur la pile*/
-        fmulp /* multiplie pi * raduis * raduis */
-        /* Write your solution here */
+        fld factor /*mise sur la pile de 2 qui ira apres l'appel de perimetre en st[1] */
+        call *12(%edi) /*appel de la methode */
+        fdivp /*divise st[0] et st[1]*/
+        fstp -4(%ebp) /*met le resulat de la division dans p*/
+
+
+        fld -4(%ebp) /*remet p sur la pile de float*/
+        fld (%eax) /*met msides[0] sur la pile de float*/
+        fsubrp /*fait (p - msides[0]) ou (st[1] - st[0])*/
+        fstp -8(%ebp)
+
+
+        fld -4(%ebp) /*remet p sur la pile de float*/
+        fld 4(%eax) /*met msides[1] sur la pile de float*/
+        fsubrp /*fait (p - msides[1]) ou (st[1] - st[0])*/
+        fld -8(%ebp) /*remet le resultat de (p - msides[0]) sur la pile de float*/
+        fmulp /* (p - msides[0])*(p - msides[1]) */
+        fstp -8(%ebp)
+
+        fld -4(%ebp) /*remet p sur la pile de float*/
+        fld 8(%eax) /*met msides[2] sur la pile de float*/
+        fsubrp /*fait (p - msides[2]) ou (st[1] - st[0])*/
+        fld -8(%ebp) /*remet le resultat de (p - msides[0]) sur la pile de float*/
+        fmulp /* (p - msides[0])*(p - msides[1])* (p - msides[2]) */
         
+        fld -4(%ebp) /*remet p sur la pile de float*/
+        fmulp /* p*(p - msides[0])*(p - msides[1])* (p - msides[2]) */
+
+        fsqrt /*renvoie la racine du resultat precedent*/
+        
+        addl $8, %esp
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
--- inf1600_tp4/triangle_height.s
+++ inf1600_tp4/triangle_height.s
@@ -1,10 +1,28 @@
+.data
+        factor: .float 2.0 /* use this to multiply by two */
+
+.text
+
 .globl	_ZNK9CTriangle9HeightAsmEv
 
 _ZNK9CTriangle9HeightAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        /* Write your solution here */
+        addl $-4, %esp /*espace pour la variable local A*/
+        mov 8(%ebp), %edi /*sauvegarde de this*/
+        lea 4(%edi), %eax /*sauvegarde de l'adresse du tableau mSide*/
+        push %edi /*sauvegarde this dans la pile*/
+        mov (%edi), %edi /*vtable adresse*/
+        call *20(%edi) /*appel de la methode */
+
+        fld 8(%eax) /*met mside[2] sur la pile de float*/
+        fdivrp /* st[1] / st[0] ou (A / mside[2]) */
+
+        fld factor /*met 2 sur la pile de float*/
+        fmulp /* (2.0 * A / mside[2])*/
+
+        add $4, %eax /*depile la variable local*/
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
